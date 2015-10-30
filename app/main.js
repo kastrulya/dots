796a494b5a97c966eea0b512bd17b6f802ac9cc1
kastrulya <julia.hrihorieva@gmail.com>
2015-10-31 00:49:13 +0200

    requirejs

---
+++ app/main.js
@@ -0,0 +1,7 @@
+define(function (require) {
+    // Load any app-specific modules
+    // with a relative require call,
+    // like:
+    var dots = require('./dots');
+    var a = require('./a');
+});
